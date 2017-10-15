a6a3bb450ec63dbab4c3782fa5e08eb90fe6dfd5
Rob White <rob.j.white@gmail.com>
2017-10-15 21:02:30 +0100

    Empty Azure Function - Testing Deployment

---
+++ PH.Contract/Internal/PasswordHashed.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Net;
+
+namespace PH.Contract.Internal
+{
+    public class PasswordHashed
+    {
+        public string HashMethod { get; set; }
+        public string PlainText { get; set; }
+        public IPAddress IpAddress { get; set; }
+        public DateTime HashRequestTimeUtc { get; set; }
+    }
+}
---
+++ PH.Functions/TopicTrigger.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Azure.WebJobs.Host;
+using PH.Contract.Internal;
+
+namespace PH.Functions
+{
+    public class TopicTrigger
+    {
+        public static void Run(PasswordHashed @event, TraceWriter log)
+        {
+            log.Info($"ServiceBus topic trigger function processed message: {@event}");
+        }
+    }
+}
