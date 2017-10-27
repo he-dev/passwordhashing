bfecf896fca9e27b7b052cf47a148c79d0babac0
Rob White <rob.j.white@gmail.com>
2017-10-27 15:10:39 +0100

    Add Unique Page Titles

---
+++ PH.Web/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace PH.Web
+{
+    public static class StringExtensions
+    {
+        public static string Left(this string value, int maxLength)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                return value;
+            }
+
+            return value.Length <= maxLength
+                ? value
+                : value.Substring(0, maxLength);
+        }
+    }
+}
